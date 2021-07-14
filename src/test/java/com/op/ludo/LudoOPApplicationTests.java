97cac766f86cefeade0ea14dba1607d311dba0cb
Sahil Chaddha <sahilvs000@gmail.com>
2021-07-14 23:17:22 +0530

    Initial Commit

---
+++ src/test/java/com/op/ludo/LudoOPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.op.ludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoOPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
